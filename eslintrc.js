020b832fc1179f9bbf8e7d8baa325735dc2705c5
GuillaumeBarreau <guillaume.barreau.z@gmail.com>
2022-05-21 13:17:45 +0200

    update

--- eslintrc.js
+++ eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
   extends: [
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended'
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: ['react', '@typescript-eslint', 'jest'],
+  plugins: ["react", "@typescript-eslint", "jest"],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: ["@typescript-eslint/parser", "babel/eslint-parser"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
   ignorePatterns: ["./dist/*"],
   rules: {
-    'linebreak-style': 'off',
-    'import/no-anonymous-default-export': 'off',
-    "max-len": ["warn", { "code": 80 }],
-    'prettier/prettier': [
-      'error',
+    "linebreak-style": "off",
+    "import/no-anonymous-default-export": "off",
+    "max-len": ["warn", { code: 80 }],
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'auto',
+        endOfLine: "auto",
       },
     ],
   },
